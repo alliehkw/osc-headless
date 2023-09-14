ac1cf360b06b6b6b0133ecb73bb7a18150eab07b
Allie Soldau <alliesoldau@gmail.com>
2023-09-14 14:09:22 -0400

    implemented scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/navbar/NavBar.js";
 import Content from "./components/content/Content.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
+import ScrollToTop from "./components/reusables/ScrollToTop.js";
 
 const app_data = gql`
   {
@@ -59,6 +60,7 @@ function App() {
       {!loading ? (
         <div className="App">
           <Router>
+            <ScrollToTop />
             <NavBar menu_data={data.menuItems.edges} />
             <Content content_data={data.pages.nodes} />
           </Router>
